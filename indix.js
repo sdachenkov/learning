ad76a8777ab6de7f88ae2c6a4fdca0a716a26a35
Sergey Dachenkov <serg@Sergeys-MacBook-Pro.local>
2021-05-08 23:16:05 +0300

    lesson1

--- indix.js
+++ indix.js
@@ -1,3 +1,20 @@
+"use strict"
+const num = 50;
+(num == 49) ? console.log('ok') : console.log('error');
+
+switch (num) {
+    case 49:
+        alert('ok');
+        break;
+    case 50:
+        alert('eeee');
+        break;
+    default:
+        alert('В другой раз');
+
+}
+
+
 const numberofFilms = +prompt('Сколько фильмов вы уже смотрели?', '');
 const personalMovieDB = {
     count: numberofFilms,
